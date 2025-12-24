c9190fcbce8c61644c1ec6e6b545e5b9cb6b28f4
ZetaGo-Aurum <deltaastra24@gmail.com>
2025-12-25 00:17:21 +0700

    Delete AloPantest directory

--- AloPantest/alo_pantest/core/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Core modules for AloPantest framework"""
-from .logger import setup_logger
-from .config import Config
-from .exceptions import AloPantestException
-
-__all__ = ['setup_logger', 'Config', 'AloPantestException']
--- AloPantest/alo_pantest/modules/crypto/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Empty __init__ for crypto module"""
